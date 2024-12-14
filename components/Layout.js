233774ae959441b2eea520f2bef1677cdee54907
Lucas-Bispo <lukaobispo@gmail.com>
2024-12-14 12:32:56 -0300

    :rotating_light: Corrigir avisos do compilador/linter.

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,15 @@
-const Layout = () => {
-  return <div>Layout</div>;
-};
+import { Sora } from 'next/font/google';
 
-export default Layout;
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+export default function Layout({ children }) {
+  return (
+    <div className={sora.variable}>
+      {children}
+    </div>
+  );
+}
